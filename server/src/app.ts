c5860bb35ea5fbcef332e00451ec3382d0eb3b72
Lost <parria.ssc@gmail.com>
2026-05-27 16:51:38 -0500

    Initialize database on app startup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 import cors from 'cors';
 import express from 'express';
 
+import { initializeDatabase } from './db/schema.js';
+
 export const createApp = () => {
   const app = express();
+
+  initializeDatabase();
 
   app.use(cors());
   app.use(express.json());
